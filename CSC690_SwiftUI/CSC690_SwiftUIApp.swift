09cc0325ddfab52ca56075233889614c54add76c
Cameron Robinson <cameron.k.robinson@gmail.com>
2021-10-25 12:19:03 -0700

    Initial Commit

---
+++ CSC690_SwiftUI/CSC690_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSC690_SwiftUIApp.swift
+//  CSC690_SwiftUI
+//
+//  Created by Cameron Robinson on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct CSC690_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSC690_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CSC690_SwiftUI
+//
+//  Created by Cameron Robinson on 10/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
